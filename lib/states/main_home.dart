9b94801511e1419a12526c673a8d8bf117254aaa
masterUNG <phrombutr@gmail.com>
2025-06-16 12:39:38 +0700

    update 16june

--- lib/states/main_home.dart
+++ lib/states/main_home.dart
@@ -1,14 +1,19 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getwidget/getwidget.dart';
 import 'package:shoppingonline/utility/app_constant.dart';
 import 'package:shoppingonline/utility/app_controller.dart';
+import 'package:shoppingonline/utility/app_dialog.dart';
 import 'package:shoppingonline/utility/app_service.dart';
 import 'package:shoppingonline/widgets/body_cart.dart';
 import 'package:shoppingonline/widgets/body_order.dart';
 import 'package:shoppingonline/widgets/body_profile.dart';
 import 'package:shoppingonline/widgets/body_shopping.dart';
+import 'package:shoppingonline/widgets/widget_button.dart';
 import 'package:shoppingonline/widgets/widget_image_asset.dart';
+import 'package:shoppingonline/widgets/widget_text.dart';
+import 'package:shoppingonline/widgets/widget_text_rich.dart';
 
 class MainHome extends StatefulWidget {
   const MainHome({super.key});
@@ -27,12 +32,7 @@ class _MainHomeState extends State<MainHome> {
     Icons.credit_card,
     Icons.person_outline
   ];
-  var bodys = <Widget>[
-    BodyShopping(),
-    BodyCart(),
-    BodyOrder(),
-    BodyProfile()
-  ];
+  var bodys = <Widget>[BodyShopping(), BodyCart(), BodyOrder(), BodyProfile()];
 
   List<BottomNavigationBarItem> items = [];
 
@@ -60,12 +60,43 @@ class _MainHomeState extends State<MainHome> {
                   Row(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      WidgetImageAsset(
-                          pathImage: 'images/logo.png', width: 72),
+                      WidgetImageAsset(pathImage: 'images/logo.png', width: 72),
                       Text(
                         'Shopping Online',
                         style: AppConstant.h2Style(fontSize: 18),
                       ),
+                      Spacer(),
+                      appController.indexBody.value == 3
+                          ? WidgetButton(
+                              text: 'SignOut',
+                              onPressed: () {
+                                AppDialog().normalDialog(
+                                    icon: WidgetImageAsset(
+                                        pathImage: 'images/intro3.png'),
+                                    title: WidgetText(
+                                      text: 'Confirm SignOut',
+                                      textStyle: AppConstant.h2Style(),
+                                    ),
+                                    content: WidgetTextRich(
+                                        head: 'Confirm Signout : ',
+                                        body: 'Tap Confirm Button'),
+                                    firstAction: WidgetButton(type: GFButtonType.outline2x,
+                                      text: 'Confirm',
+                                      onPressed: () async {
+                                        await FirebaseAuth.instance
+                                            .signOut()
+                                            .whenComplete(
+                                          () {
+                                            Get.delete();
+                                            Get.offAllNamed('/signinsignup');
+                                          },
+                                        );
+                                      },
+                                    ));
+                              },
+                              type: GFButtonType.outline)
+                          : SizedBox(),
+                      SizedBox(width: 16),
                     ],
                   ),
                 ],
